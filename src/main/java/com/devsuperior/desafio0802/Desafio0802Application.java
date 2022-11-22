b87101d5a29605e1c5dee26a3e5cf470d0e5429f
Douglas Fabiano <douglasfabiano23@gmail.com>
2022-11-22 20:41:04 -0300

    Criadas as entidades Participante, Atividade, Categoria e Bloco

---
+++ src/main/java/com/devsuperior/desafio0802/Desafio0802Application.java
@@ -0,0 +1,14 @@
+package com.devsuperior.desafio0802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio0802Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Desafio0802Application.class, args);
+	}
+
+}
